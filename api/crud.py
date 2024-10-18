e6ca5e24b91f7e0e7f2ba7880d275d9eb42a88f4
Cristian Hernandez <crizh221@gmail.com>
2024-10-17 20:31:20 -0500

    weather-station

---
+++ api/crud.py
@@ -0,0 +1,10 @@
+'''
+C=> Create  (INSERT INTO)
+R=> Read    (SELECT)
+U=> Update  (UPDATE)
+D=> Delete  (DELETE)
+'''
+
+
+def create_user():
+    new_user_query="INSERT INTO user ()"
---
+++ api/get_arduino_data.py
@@ -0,0 +1,35 @@
+'''
+Script description:
+Get temperature and humidity from DHT11 since Arduino.
+Date: 07-10-2024
+Developer:  Cristian Hernandez
+'''
+
+#Import libraries 
+import serial
+import time
+
+#Arduino port
+arduino_port = 'COM13'
+arduino_bau = 9600
+
+service = serial.Serial(
+    arduino_port,
+    arduino_bau,
+    timeout=1
+)
+
+time.sleep(1) #delay
+
+while True:
+    #data = service.readline.decode('utf-8').strip()
+    data = service.readline().decode('utf-8').rstrip()
+    
+    if data:
+        print(data)
+        #temperature, humidity = data.split(",")
+        
+        #print(f"Temperature: {temperature}°C")
+        #print(f"Humidity: {humidity}%")
+        
+    time.sleep(1)
